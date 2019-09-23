02be2d88fb5de4407c151b49b273f3635c2998ee
Errmakovv <darkday3@mail.ru>
2019-09-23 22:37:18 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-module.exports = function zeros(expression) {
-  // your solution
-}
+module.exports = function zeros(exp) {
+    let part = exp.split("*");
+    let two=0;
+    let five=0;
+
+    part.map(elem => {
+      let period,
+        num;
+  
+      if (elem[elem.length-1]=='!' && elem[elem.length-2]=='!') {
+        period = 2;
+        num = +elem.slice(0, -2);
+      } else {
+        period = 1;
+        num = +elem.slice(0, -1);
+      }
+      for(; 0 < num ; num -= period) {
+        let temp=num;
+        while (temp % 5 == 0) { 
+          temp /= 5;
+          five++;
+        }
+      temp=num;
+        while (temp % 2 == 0) {
+          temp /= 2;
+          two++;
+        }
+      }
+    });
+
+    return Math.min(two, five);
+  }
+
